0327bc65aba22b3b5db224bef0db6a9ad70af306
Lidianne Barbosa <lidiannerb@gmail.com>
2022-06-19 22:23:50 -0300

    feat: structuring the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./Components/Pages/Home";
+import Contact from "./Components/Pages/Contact";
+import Company from "./Components/Pages/Company";
+import NewProject from "./Components/Pages/NewProject";
+
+import Container from "./Components/Layout/Container";
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/Company">Company</Link>
+        <Link to="/NewProject">NewProject</Link>
+      </div>
+      <Container customClass="min_height">
+        <Routes>        
+          <Route path="/" element={<Home />} />
+          <Route path="/Company" element={<Company />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/NewProject" element={<NewProject />} />        
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/Components/Pages/Company.jsx
@@ -0,0 +1,9 @@
+const Company = () => {
+  return (
+    <>
+    <h1>Company</h1>
+    </>
+  );
+}
+ 
+export default Company;
