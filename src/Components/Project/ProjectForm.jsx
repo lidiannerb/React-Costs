deab0addba29d1cd9924e6cd1b015be334579144
Lidianne Barbosa <lidiannerb@gmail.com>
2022-06-21 09:38:47 -0300

    feat: connecting API

--- src/Components/Project/ProjectForm.jsx
+++ src/Components/Project/ProjectForm.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+
 import Input from "../Form/Input";
 import Select from "../Form/Select";
 import SubmitButton from "../Form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
 const ProjectForm = ({btnText}) => {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application=json"
+      },
+    })
+      .then((resposta) => resposta.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch((erro) => console.log(erro))
+  },[])
+
   return (
     <form className={styles.form}>
       <Input
@@ -20,7 +39,9 @@ const ProjectForm = ({btnText}) => {
       />
       <Select 
         name="categoty_id" 
-        text="Selecione a categoria" />
+        text="Selecione a categoria" 
+        options={categories}
+      />
       <SubmitButton text={btnText}/>
     </form>
   );
@@ -29,9 +50,8 @@ const ProjectForm = ({btnText}) => {
 export default ProjectForm;
 
 
-// O SubmitButton tem o texto dinamico, a prop (btnText) que está sendo passada e ele vem 
-// do componente pai ProjectForm
+// O SubmitButton tem o texto dinamico, que é a prop btnText que está sendo passada 
+// do componente pai (ProjectForm)
 // Essa propriedade está sendo usada na page NewProject, pois é lá que este botão está sendo usado para 
 // criar um novo projeto, então o texto só faz sentido lá na page NewProject
  
-// Resumo: 
