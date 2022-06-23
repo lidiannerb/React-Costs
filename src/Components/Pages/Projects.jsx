4a1a12db9a153a070983139aff5e1da7f62de79a
Lidianne Barbosa <lidiannerb@gmail.com>
2022-06-23 09:37:18 -0300

    feat: creating dashboard to receive the projects

--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -1,19 +1,28 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../Layout/Message";
+import Container from "../Layout/Container";
+import LinkButton from "../Layout/LinkButton";
+
+import styles from "./Projects.module.css";
 
 const Projects = () => {
   const location = useLocation()
   let message = ""
   if (location.state) {
     message = location.state.message
-  }
-  
+  }  
 
   return ( 
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/NewProject" text="Criar Projeto" />
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }
